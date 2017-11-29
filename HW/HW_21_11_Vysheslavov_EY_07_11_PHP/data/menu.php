3d1bcfd91eab88d3a9fbac0027c2ed46e12935a7
v-e-y <v-e-y@outlook.com>
2017-11-29 21:50:21 +0200

    add base html,  showed errors code, require menu code. refactor menu data. add recursive function

--- HW/HW_21_11_Vysheslavov_EY_07_11_PHP/data/menu.php
+++ HW/HW_21_11_Vysheslavov_EY_07_11_PHP/data/menu.php
@@ -1,48 +1,77 @@
 <?php
 /*
 * PATH: /HW/HW_21_11_Vysheslavov_EY_07_11_PHP/data/
-*
 */
 
+// TODOS del dec comments
 
+
+// Example data for menu
 // TODO Change variable to return, variable for dev
 $devArr = [
     [
         'id' => 0,
+        'parentId' => false,
         'title' => 'First item',
         'href' => '/#1',
         'child' => false
     ],
     [
         'id' => 1,
+        'parentId' => false,
         'title' => 'Second item',
         'href' => '/#2',
         'child' => false
     ],
     [
         'id' => 2,
+        'parentId' => false,
         'title' => 'Third item',
         'href' => '/#3',
         'child' => true,
         'childItems' => [
             [
                 'id' => 3,
+                'parentId' => 2,
                 'title' => 'Some item',
                 'href' => '/#4',
                 'child' => false
             ],
             [
                 'id' => 4,
+                'parentId' => '2',   
                 'title' => 'One more item',
                 'href' => '/#5',
-                'child' => false
+                'child' => true,
+                'childItems' => [
+                    [
+                    'id' => 5,
+                    'parentId' => 4,
+                    'title' => 'Some one more item',
+                    'href' => '/#6',
+                    'child' => false
+                    ]
+                ]
             ],
         ]
     ],
 ];
 
-foreach ($devArr as $item) {
-    if ($item['child'] != false) {
-        print_r($item);
+
+
+function showMenuItems(array $menuArry)
+{
+    // print_r($menuArry);
+    foreach ($menuArry as $menuArryItem) {
+        if ($menuArryItem['child'] == false) {
+            echo $menuArryItem['id']; 
+        }
+        if ($menuArryItem['child'] == true) {
+            echo $menuArryItem['id'];
+            showMenuItems($menuArryItem['childItems']);
+        }
     }
 }
+
+
+showMenuItems($devArr);
