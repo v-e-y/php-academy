61ea518042d3690da5ee98ca51664da2505d766c
Edward V <Edward V>
2017-12-02 22:35:21 +0200

    lesson 01_12

--- index.php
+++ index.php
@@ -1,25 +1,6 @@
 <?php
-// Vysheslavov Euard, email: v-e-y@outlook.com
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
-$someArray = [
-    'first'  => '1',
-    'second' => '2',
-    'third'  => '3',
-    'nextArray' => [
-        'first'  => '41',
-        'second' => '42',
-        'third'  => '43',
-    ]
-];
 
-// echo $someArray.nextArray;
-// array_map()
-// sprintf('%s %s', ...$someArray);
-// print_r(count($someArray));
-// echo count($someArray, COUNT_RECURSIVE);
 ?>
 
 
---
+++ lessons/01_12/index.php
@@ -0,0 +1,18 @@
+<?php
+/*
+- abra tehnologi
+- bee jee
+- ben time (bee in time)
+
+- git rebase git merge
+- ssl 443
+*/
+
+
+ function save(string $name, ...$soname) {
+     $test = [];
+     $str = 'ssdfsdf';
+    echo __FUNCTION__;
+ }
+ save('Ed', 'werwr', '2342344');
+
