6fe9cea3aef9f8a3fdf015c7316e4a3bdaeb1157
v-e-y <v-e-y@outlook.com>
2017-12-06 23:26:21 +0200

    add 1 task and his core sheme

---
+++ HW/HW_05_12Vysheslavov_EY_07_11_PHP/functions_forms_tasks/1.php
@@ -0,0 +1,35 @@
+<?php
+/*
+* Vysheslavov E.Y. (07_11_PHP) [v-e-y@outlook.com]
+* Home work from 05.12
+* Task: 1. Создать форму с двумя элементами textarea. При отправке формы скрипт должен выдавать только те слова, которые есть и в первом и во втором поле ввода.
+*          Реализацию это логики необходимо поместить в функцию getCommonWords($a, $b), которая будет возвращать массив с общими словами.
+*/
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Two forms with textarea</title>
+</head>
+<body>
+    <form action="" method="post" role="form"></form>
+</body>
+</html>
+
+
+<?php
+
+function getCommonWords($a, $b) {
+    
+}
+
+?>
